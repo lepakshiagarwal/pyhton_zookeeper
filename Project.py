384b9397cf5777a72a7b87659409ba2006775bbb
lepakshiagarwal <60332064+lepakshiagarwal@users.noreply.github.com>
2020-05-17 16:41:17 -0400

    Shows an anima

--- Project.py
+++ Project.py
@@ -1,5 +1,20 @@
-print("I do love animals!")
-print("Start looking after animals...")
-print("Deer looks fine.")
-print("Bat looks happy.")
-print("Lion looks healthy.")
+print("""
+Switching on camera from habitat with camels...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Yey, our little camel is sunbathing!""")
